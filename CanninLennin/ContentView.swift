ff3a284b6308aae4b9efac714035bf680b42dcf5
Lennin sabogal <lenninssp1021@gmail.com>
2025-10-28 16:56:01 -0400

    blabla

--- CanninLennin/ContentView.swift
+++ CanninLennin/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Text("Este es un cambio importante")
         }
         .padding()
     }
