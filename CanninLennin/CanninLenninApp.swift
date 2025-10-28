9c84b26ce8c6ef2741613a5c68a06fc8d58745b8
Lennin sabogal <lenninssp1021@gmail.com>
2025-10-28 16:28:06 -0400

    Initial Commit

---
+++ CanninLennin/CanninLenninApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanninLenninApp.swift
+//  CanninLennin
+//
+//  Created by Lennin Sabogal on 28/10/25.
+//
+
+import SwiftUI
+
+@main
+struct CanninLenninApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
