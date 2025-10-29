2cd54b8b3cb062267a1defe9f3061ceef84430fc
Lennin sabogal <lenninssp1021@gmail.com>
2025-10-29 17:01:36 -0400

    dev environment setup

---
+++ CanninLennin/Backend/playground.swift
@@ -0,0 +1,9 @@
+/*
+Pages to open on every work session:
+
+open -a "Google Chrome" --new --args --new-window
+sleep 1
+open -a "Google Chrome" https://github.com/mnabaee/kernels/blob/master/mtl-street-parking/README.md
+open -a "Google Chrome" https://github.com/Lenninssp/CaninParking
+
+ */
